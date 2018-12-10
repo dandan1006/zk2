910222f27267216f437858d14d6bf7b759a49006
dandan1006 <1643060583@163.com>
2018-12-10 09:30:23 +0800

    前端起服务完毕

---
+++ zk-fdd/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp=require('gulp');
+var server=require('gulp-webserver');
+gulp.task('server',function(){
+	return gulp.src('src')
+	.pipe(server({
+		port:8888,
+		proxies:[{
+			source:'/users',
+			target:'http://localhost:3000/users'
+		}]
+	}))
+})
